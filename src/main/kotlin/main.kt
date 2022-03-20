f92c9d9d00a9c695010e9281c368a78e559526e7
Кадеркаев Даниил <ortixon@gmail.com>
2022-03-20 22:14:45 +0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val likes = 111
+    if (likes % 10 != 1 || likes % 100 == 11) {
+        println("Понравилось $likes людям")
+    }
+    else{
+        println("Понравилось $likes человеку")
+    }
+}
